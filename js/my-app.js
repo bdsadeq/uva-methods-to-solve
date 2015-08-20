0fd9917fbcda77a00c946600002120959b096048
Sadeq <Sadeq@180.234.116.33>
2015-08-20 20:35:10 +0600

    Author : Sadeq Date: 16 Aug 2015 Updated SQL : update-2015-08-16-sadeq.sql Working File : ReportsController (Line 1646,1663)

---
+++ js/my-app.js
@@ -0,0 +1,7 @@
+$$('.about_modal').on('click', function () {
+    myApp.alert('<b>Md. Sadequr Rahman</b><br/>Sr. Software Engineer (Team Leader)<br/>Dream Door Soft Ltd.<br/><small>Version 1.0.0</small>', 'About');
+});
+
+$$('.contribute_modal').on('click', function () {
+    myApp.alert('Send email to info@winexsoft.com to share your idea.', 'Contribute');
+});
